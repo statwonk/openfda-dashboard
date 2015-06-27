893121cee9b8b8e1d300184e6d8c966fe702e187
statwonk <cpeter9@gmail.com>
2015-06-27 15:56:01 -0500

    Isolate reactive plotting functions

--- data.R
+++ data.R
@@ -30,82 +30,89 @@ count_fda <- function(variable, ...) {
 dates_received <- reactive({
   if(is.null(input$drug) | input$run_button == 0)
     return()
+  isolate({
+    d <- count_fda(variable = "receivedate",
+                   input$drug)
 
-  d <- count_fda(variable = "receivedate",
-                 input$drug)
+    d <- d %>%
+      mutate(time = as.POSIXct(time,
+                               format = "%Y%m%d"))
 
-  d <- d %>%
-    mutate(time = as.POSIXct(time,
-                             format = "%Y%m%d"))
+    # One possible extention is to add a grouping
+    # toggle like: month, week, day
+    d <- d %>%
+      mutate(time = cut.POSIXt(time, 'week',
+                               start.on.monday = F)) %>%
+      group_by(drug, time) %>%
+      summarise(count = sum(count)) %>%
+      mutate(time = as.character(time),
+             time = as.POSIXct(time))
 
-  # One possible extention is to add a grouping
-  # toggle like: month, week, day
-  d <- d %>%
-    mutate(time = cut.POSIXt(time, 'week',
-                             start.on.monday = F)) %>%
-    group_by(drug, time) %>%
-    summarise(count = sum(count)) %>%
-    mutate(time = as.character(time),
-           time = as.POSIXct(time))
+    # There are trade-offs in terms of counting weeks
+    # without adverse events as 0 or NA.
+    # Splines work in log-scale if weeks without events
+    # are set to NA. In general, counting 0s are
+    # important for distributional modeling and even smoothing.
+    # For many drugs 0 counts are rare in given weeks.
 
-  # There are trade-offs in terms of counting weeks
-  # without adverse events as 0 or NA.
-  # Splines work in log-scale if weeks without events
-  # are set to NA. In general, counting 0s are
-  # important for distributional modeling and even smoothing.
-  # For many drugs 0 counts are rare in given weeks.
+    #   date_sequence <- data.frame(
+    #     time = as.POSIXct(
+    #       seq(min(d$time),
+    #           max(d$time),
+    #           by = "week"),
+    #       format = "%Y-%m-%d"))
+    #
+    #   d <- left_join(date_sequence,
+    #                  d) %>%
+    #     mutate(count = ifelse(is.na(count), 0, count))
 
-  #   date_sequence <- data.frame(
-  #     time = as.POSIXct(
-  #       seq(min(d$time),
-  #           max(d$time),
-  #           by = "week"),
-  #       format = "%Y-%m-%d"))
-  #
-  #   d <- left_join(date_sequence,
-  #                  d) %>%
-  #     mutate(count = ifelse(is.na(count), 0, count))
-
-  return(d)
+    return(d)
+  })
 })
 
 ages <- reactive({
   if(is.null(input$drug) | input$run_button == 0)
     return()
-  count_fda(variable = "patient.patientonsetage",
-            input$drug)
+  isolate({
+    count_fda(variable = "patient.patientonsetage",
+              input$drug)
+  })
 })
 
 outcomes <- reactive({
   if(is.null(input$drug) | input$run_button == 0)
     return()
-  as.data.frame(
-    tbl_df(
-      dcast(
-        count_fda(variable = "patient.reaction.reactionoutcome",
-                  input$drug),
-        term ~ drug,
-        value.var = "count"
-      )
-    ) %>%
-      mutate(term = sapply(term, function(x) {
-        switch(x,
-               "Recovered/resolved",
-               "Recovering/resolving",
-               "Not recovered/not resolved",
-               "Recovered/resolved with sequelae",
-               "Fatal",
-               "Unknown")})) %>%
-      rename(Outcome = term)
-  )
+  isolate({
+    as.data.frame(
+      tbl_df(
+        dcast(
+          count_fda(variable = "patient.reaction.reactionoutcome",
+                    input$drug),
+          term ~ drug,
+          value.var = "count"
+        )
+      ) %>%
+        mutate(term = sapply(term, function(x) {
+          switch(x,
+                 "Recovered/resolved",
+                 "Recovering/resolving",
+                 "Not recovered/not resolved",
+                 "Recovered/resolved with sequelae",
+                 "Fatal",
+                 "Unknown")})) %>%
+        rename(Outcome = term)
+    )
+  })
 })
 
 
 reactionoutcomes <- reactive({
   if(is.null(input$drug) | input$run_button == 0)
     return()
-  dcast(
-    count_fda(variable = "patient.reaction.reactionmeddrapt",
-              input$drug),
-    term ~ drug, value.var = "count")
+  isolate({
+    dcast(
+      count_fda(variable = "patient.reaction.reactionmeddrapt",
+                input$drug),
+      term ~ drug, value.var = "count")
+  })
 })
